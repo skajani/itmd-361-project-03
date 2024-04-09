9e6feed43627621b8960d422a49297d0e78cf0a7
skajani <skajani@hawk.iit.edu>
2024-04-09 17:41:17 -0500

    Added Benny the Bull Icon & created map js info

--- js/ucmap.js
+++ js/ucmap.js
@@ -1,36 +1,38 @@
 function init(){
-	alert('it works');
-	// var el = document.getElementById('canvas');
-	// var myLocation = new google.maps.LatLng(41.835117, -87.627130);
-	// var mapOptions = {
-	// 	center: myLocation,
-	// 	zoom: 18,
-	// 	mapTypeId: google.maps.MapTypeId.SATELLITE,
-	// 	mapTypeControlOptions: {
-	// 		position: google.maps.ControlPosition.BOTTOM_CENTER
-	// 	}
-	// };
+  // alert('Test to see if it works');
+  var el = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(41.88129788592486, -87.67421881581986); 
+  var mapOptions = {
+    center: myLocation,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+    position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
 
-	// var myMap = new google.maps.Map(el, mapOptions);
+  var myMap = new google.maps.Map(el, mapOptions);
 
-	// var marker = new google.maps.Marker({
-	// 	position: myLocation,
-	// 	map: myMap,
-	// 	animation: google.maps.Animation.BOUNCE,
-	// 	icon: 'iit-icon.png'
-	// });
+  var marker = new google.maps.Marker({
+    position: myLocation,
+    map: myMap,
+	animation: google.maps.Animation.BOUNCE,
+	icon: {
+      url: 'images/benny_the_bull.png',
+      scaledSize: new google.maps.Size(100,100)
+    } 
+  });
 
-	// var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odit optio, voluptatem placeat odio dignissimos illo magnam esse asperiores voluptas at iure vero eum, nemo aperiam? Ipsam, atque nobis rem.</p>';
+  var contentString = '<h1>Untied Center</h1><p>United Center is located at 1901 W Madison St, Chicago IL. This is home turf for the Chicago Bulls (my favorite basketball team) & Chicago Blackhawks. Many concerts are also held here.</p>';
 
-	// var infowindow = new google.maps.InfoWindow({
-    //   content: contentString
-  	// });
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
 
-	// google.maps.event.addListener(marker, 'mouseover', function() {
-    // 	infowindow.open(myMap, marker);
-  	// });
-
-
+  google.maps.event.addListener(marker, 'mouseover', function() {
+    infowindow.open(myMap, marker);
+  });
+  
 }
 
 google.maps.event.addDomListener(window, 'load', init);
